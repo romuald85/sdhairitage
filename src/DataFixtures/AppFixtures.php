10665013d96c10acd8ed5b30622d2109fad0d294
romuald85 <papeboyedk@gmail.com>
2021-01-07 15:50:08 +0100

    Create Product and Category Entities and make relation between

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,47 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Product;
+use Liior\Faker\Prices;
+use App\Entity\Category;
+use Bezhanov\Faker\Provider\Commerce;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
+    private $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+
+        for ($c = 0; $c < 3; $c++) {
+            $category = new Category;
+            $category->setName($faker->department());
+            $category->setSlug(strtolower($this->slugger->slug($category->getName())));
+
+            $manager->persist($category);
+
+            for ($p = 0; $p < mt_rand(15, 20); $p++) {
+                $product = new Product;
+                $product->setName($faker->productName())
+                    ->setPrice($faker->price(1000, 6000))
+                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    ->setCategory($category);
+                $manager->persist($product);
+            }
+        }
+
 
         $manager->flush();
     }
