eab1d41d36245f0cd9bfc06a57c114a9ae66bfb4
romuald85 <papeboyedk@gmail.com>
2021-01-08 22:06:29 +0100

    Add category and show functions in ProductController and add debug bundle

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,6 +19,43 @@ class ProductController extends AbstractController
 
         return $this->render('product/product.html.twig', [
             'products' => $products
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}", name="product_category")
+     */
+    public function category($slug, CategoryRepository $categoryRepository)
+    {
+        $category = $categoryRepository->findOneBy([
+            'slug' => $slug
+        ]);
+
+        if (!$category) {
+            throw $this->createNotFoundException("La catégorie demandée n'existe pas");
+        }
+
+        return $this->render('product/category.html.twig', [
+            'slug' => $slug,
+            'category' => $category
+        ]);
+    }
+
+    /** 
+     * @Route("/{category_slug}/{slug}", name="product_show")
+     */
+    public function show($slug, ProductRepository $productRepository)
+    {
+        $product = $productRepository->findOneBy([
+            'slug' => $slug
+        ]);
+
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit demandé n'existe pas");
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
