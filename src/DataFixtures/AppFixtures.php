6f3723df3632e2a1b63f4795687f027d4b53d5b8
romuald85 <papeboyedk@gmail.com>
2021-01-15 22:47:01 +0100

    Install bundle security, create user table and 5 fixture users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Product;
 use Liior\Faker\Prices;
 use App\Entity\Category;
@@ -27,6 +28,24 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+
+        $admin = new User;
+
+        $admin->setEmail("admin@gmail.com")
+            ->setPassword("password")
+            ->setFullName("Admin")
+            ->setRoles(["ROLE_ADMIN"]);
+
+        $manager->persist($admin);
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User();
+            $user->setEmail("user$u@gmail.com")
+                ->setFullName($faker->name())
+                ->setPassword("password");
+
+            $manager->persist($user);
+        }
 
         for ($c = 0; $c < 3; $c++) {
             $category = new Category;
