e7975241dba33f0cc3d81fcacc1e06d5072f6c43
romuald85 <papeboyedk@gmail.com>
2021-01-17 10:04:38 +0100

    Change message Invalid credentials and feature logout and login buttons

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -22,4 +22,11 @@ class SecurityController extends AbstractController
             'error' => $utils->getLastAuthenticationError()
         ]);
     }
+
+    /** 
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout()
+    {
+    }
 }
