8c19b27e12a1311aa8738581defb21b9ede40eae
romuald85 <papeboyedk@gmail.com>
2021-01-19 22:25:11 +0100

    Create CartController and add function

---
+++ src/Controller/CartController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/cart/add/{id}", name="cart_add", requirements={"id":"\d+"})
+     */
+    public function add($id, SessionInterface $session, ProductRepository $productRepository)
+    {
+        $product = $productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit $id n'existe pas !");
+        }
+
+        $cart = $session->get('cart', []);
+
+        if (array_key_exists($id, $cart)) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        $session->set('cart', $cart);
+
+        /** @var FlashBag*/
+        $flashes = $session->getBag('flashes');
+
+        $flashes->add("success", "Le produit a bien été ajouté au panier");
+
+        return $this->redirectToRoute('product_show', [
+            'category_slug' => $product->getCategory()->getSlug(),
+            'slug' => $product->getSlug()
+        ]);
+    }
+}
